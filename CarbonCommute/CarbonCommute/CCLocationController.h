d84cb9d2e89e15c9dffd7b3e7518a1c140a2deb4
Chris Elsmore <elsmorian@gmail.com>
2012-07-17 11:48:45 +0100

    Start of region monitoring code

---
+++ CarbonCommute/CarbonCommute/CCLocationController.h
@@ -0,0 +1,13 @@
+//
+//  CCLocationController.h
+//  CarbonCommute
+//
+//  Created by Chris Elsmore on 17/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCLocationController : NSObject
+
+@end
---
+++ CarbonCommute/CarbonCommute/CCLocationController.m
@@ -0,0 +1,13 @@
+//
+//  CCLocationController.m
+//  CarbonCommute
+//
+//  Created by Chris Elsmore on 17/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCLocationController.h"
+
+@implementation CCLocationController
+
+@end
