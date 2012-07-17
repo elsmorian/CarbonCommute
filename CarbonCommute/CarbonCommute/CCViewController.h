11581c91be98f343d12a4cb44dc5f13ec1461a07
Chris Elsmore <elsmorian@gmail.com>
2012-07-17 10:49:17 +0100

    Added initail XCode project

---
+++ CarbonCommute/CarbonCommute/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  CarbonCommute
+//
+//  Created by Chris Elsmore on 17/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ CarbonCommute/CarbonCommute/CCViewController.m
@@ -0,0 +1,34 @@
+//
+//  CCViewController.m
+//  CarbonCommute
+//
+//  Created by Chris Elsmore on 17/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCViewController.h"
+
+@interface CCViewController ()
+
+@end
+
+@implementation CCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+  return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
